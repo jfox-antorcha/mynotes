5eb25add97e60815daf6e2af5838dcaf5cbb3fec
Jonathan Fox <foxj.dev@gmail.com>
2023-01-17 19:31:11 -0300

    feat: crud local storage sqlite

---
+++ lib/services/crud/notes_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectoryException implements Exception {}
+
+class DatabasaIsNotOpenException implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlredyExistsException implements Exception {}
+
+class CouldNotFindUserException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
+
+class CouldNotFindNoteException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
